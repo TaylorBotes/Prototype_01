3266af0086ca27114ea7dd0ddec79c5a34a2e1e5
TaylorBotes <2345240@students.wits.ac.za>
2024-06-07 20:56:35 +0200

    Update PowerUpManager.cs

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -8,7 +8,7 @@ public class PowerUpManager : MonoBehaviour
     int randomNum_2;
 
     [SerializeField]
-    public Transform boardParent;
+    public Transform boardParent; //reference to the parent of the tiles
 
     Transform Tile1;
     Transform Tile2;
@@ -21,22 +21,35 @@ public class PowerUpManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        randomNum_1 = Random.Range(1,47);
+        //finds a random number from the list of tiles (excluding the first and last. Lists start at 0 so the tiles would be 0-48).
+        randomNum_1 = Random.Range(1,47); 
         randomNum_2 = Random.Range(1,47);
 
 
-        Tile1 = boardParent.GetChild(randomNum_1);
+        //Sets a reference to the randmo tile we found using random.range
+        Tile1 = boardParent.GetChild(randomNum_1); 
         Tile2 = boardParent.GetChild(randomNum_2);
 
-        TileList.Add(Tile1);
+        //Added it to a list so I can loop through it using a for loop.
+        TileList.Add(Tile1); 
         TileList.Add(Tile2);
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 2; i++) // you can say i<TileList.Count instead of 2.
         {
-
+            //Using the list infor to get position
+            //I parented it to the object this script is attached to so I would be able to set the object active as I deactivated it in the scene.
+            //That step (and the one setting the children active) wouldn't be necessary if you use an actual power up prefab.
             Instantiate(powerUp_prefab, TileList[i].position, TileList[i].rotation, this.transform);
+
+
             this.transform.GetChild(i).gameObject.SetActive(true);
-            Debug.Log("Instantiation code");
+            Debug.Log("Instantiation code"); //checking
+            Debug.Log(i);
         }
+    }
+
+    private void Update()
+    {
+
     }
 }
